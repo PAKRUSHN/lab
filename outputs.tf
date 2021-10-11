3135a89d867f79a9a3a4749c3097c9f2c2172766
PAKRUSHN <90865353+PAKRUSHN@users.noreply.github.com>
2021-10-10 22:30:34 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,16 @@
-output "pwnbox_ip" {
-  value = aws_instance.pakrushn_node[0].public_dns
+# -- network?outputs.tf --
+
+output "vpc_id" {
+  value = aws_vpc.pakrushn_vpc.id
 }
+output "public_sg" {
+  value = aws_security_group.pakrushn_sg["public"].id
+}
+output "public_subnets" {
+  value = aws_subnet.pakrushn_public_subnet.*.id
+}
+output "juiceshop_sg" {
+  value = aws_security_group.pakrushn_sg["juiceshop"].id
+}
+
 
